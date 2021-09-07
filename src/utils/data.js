d149c6a3f3e523ba5d46c28680d262fba7666505
luizqos <luiz.qos@gmail.com>
2021-09-06 21:11:16 -0300

    Add Radios JP, Alvorada

--- src/utils/data.js
+++ src/utils/data.js
@@ -13,4 +13,25 @@ export default [
     logo: '/logo/977.png',
     file: "https://playerservices.streamtheworld.com/api/livestream-redirect/977_HITSAAC_SC",
   },
+  {
+    title: "Ouvindo",
+    station: "Alvorada",
+    radioName: 'alvorada',
+    logo: '/logo/alvorada.png',
+    file:"https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO_ALVORADAAAC.aac",
+  },
+  {
+    title: "Ouvindo",
+    station: "Jovem Pan BH",
+    radioName: 'jpbh',
+    logo: '/logo/jpbh.png',
+    file:"http://8a3c.live.upx.net.br:8036/stream.mp3",
+  },
+  {
+    title: "Ouvindo",
+    station: "Jovem Pan SP",
+    radioName: 'jpsp',
+    logo: '/logo/jpsp.png',
+    file:"https://playerservices.streamtheworld.com/api/livestream-redirect/JP_SP_FMAAC.aac",
+  },
 ];
